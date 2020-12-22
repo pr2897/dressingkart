4523054e6ed47ec55ac54a72c921f53e7a78d6b7
Piyush Raj <piyushrajkhg@gmail.com>
2020-12-23 01:38:53 +0530

    created react app from the template and added eslint & prettier support

---
+++ frontend/src/App.js
@@ -0,0 +1,135 @@
+function App() {
+  return (
+    <div class="grid-container">
+      <header class="row">
+        <div>
+          <a class="brand" href="index.html">
+            amazona
+          </a>
+        </div>
+        <div>
+          <a href="cart.html">Cart</a>
+          <a href="signin.html">Sign In</a>
+        </div>
+      </header>
+
+      <main>
+        <div class="row center">
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="images/p1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-half-o"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+              </div>
+              <div class="price">$120</div>
+            </div>
+          </div>
+
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="images/p1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-half-o"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+              </div>
+              <div class="price">$120</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="images/p1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-half-o"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+              </div>
+              <div class="price">$120</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="images/p1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-half-o"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+              </div>
+              <div class="price">$120</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="images/p1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-half-o"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+              </div>
+              <div class="price">$120</div>
+            </div>
+          </div>
+          <div class="card">
+            <a href="product.html">
+              <img class="medium" src="images/p1.jpg" alt="product" />
+            </a>
+            <div class="card-body">
+              <a href="product.html">
+                <h2>Nike Slim Shirts</h2>
+              </a>
+              <div class="rating">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star-half-o"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+              </div>
+              <div class="price">$120</div>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer class="row center">All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
