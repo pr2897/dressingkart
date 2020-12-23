5485ec457a68f35239ff46a9a41471e568a80782
Piyush Raj <piyushrajkhg@gmail.com>
2020-12-23 15:20:53 +0530

    integrated LoadingBox and MessageBox Component

--- frontend/src/screen/HomeScreen.js
+++ frontend/src/screen/HomeScreen.js
@@ -1,22 +1,41 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import LoadingBox from "../components/LoadingBox";
+import MessageBox from "../components/MessageBox";
 import Product from "../components/Product";
 
 function HomeScreen() {
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
-      const { data } = await axios.get("/api/v1/products");
-      setProducts(data.products.products);
+      try {
+        setLoading(true);
+        const { data } = await axios.get("/api/v1/products");
+        setLoading(false);
+        setProducts(data.products.products);
+      } catch (e) {
+        setError(e.message);
+        setLoading(false);
+      }
     };
     fetchData();
   }, []);
   return (
-    <div className="row center">
-      {products.map((product) => (
-        <Product product={product} key={product._id} />
-      ))}
+    <div>
+      {loading ? (
+        <LoadingBox />
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {products.map((product) => (
+            <Product product={product} key={product._id} />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
