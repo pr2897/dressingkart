b8d437ffcec7ab10f34abc70d1a50df1dd4668a7
Piyush Raj <piyushrajkhg@gmail.com>
2020-12-23 14:28:38 +0530

    added route based on screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,31 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Product from "./components/Product";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
 
 function App() {
-  const [products, setProducts] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data } = await axios.get("/api/v1/products");
-      setProducts(data.products.products);
-    };
-    fetchData();
-  }, [products]);
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">
-            amazona
-          </a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/signin">Sign In</a>
-        </div>
-      </header>
+    <Router>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              amazona
+            </a>
+          </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/signin">Sign In</a>
+          </div>
+        </header>
 
-      <main>
-        <div className="row center">
-          {products.map((product) => (
-            <Product product={product} key={product._id} />
-          ))}
-        </div>
-      </main>
+        <main>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/product/:id" component={ProductScreen}></Route>
+        </main>
 
-      <footer className="row center">All rights reserved</footer>
-    </div>
+        <footer className="row center">All rights reserved</footer>
+      </div>
+    </Router>
   );
 }
 
