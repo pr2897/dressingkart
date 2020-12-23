5c36914b6634fddaa2cf766b06d498edb1ac6381
Piyush Raj <piyushrajkhg@gmail.com>
2020-12-23 15:19:32 +0530

    added LoadingBox component

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function LoadingBox() {
+  return (
+    <div>
+      <span>
+        <i className="fa fa-spinner"></i> Loading...
+      </span>
+    </div>
+  );
+}
+
+export default LoadingBox;
