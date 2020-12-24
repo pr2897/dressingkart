aee91281eec23e05c21429bdb7fe76cc17a288e5
Piyush Raj <piyushrajkhg@gmail.com>
2020-12-24 05:34:39 +0530

    added productDetails actionCreator

--- frontend/src/redux/actions/Products/index.js
+++ frontend/src/redux/actions/Products/index.js
@@ -1,5 +1,8 @@
 import axios from "axios";
 import {
+  PRODUCT_DETAIL_FAIL,
+  PRODUCT_DETAIL_REQUEST,
+  PRODUCT_DETAIL_SUCCESS,
   PRODUCT_LIST_FAIL,
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
@@ -13,5 +16,22 @@ export const listProducts = () => async (dispatch) => {
     dispatch({ type: PRODUCT_LIST_SUCCESS, payload: data.products });
   } catch (error) {
     dispatch({ type: PRODUCT_LIST_FAIL, payload: error.message });
+  }
+};
+
+export const fetchProductDetails = (productId) => async (dispatch) => {
+  dispatch({ type: PRODUCT_DETAIL_REQUEST, payload: productId });
+  try {
+    const { data } = await axios.get(`/api/v1/product/${productId}`);
+
+    dispatch({ type: PRODUCT_DETAIL_SUCCESS, payload: data.product });
+  } catch (error) {
+    dispatch({
+      type: PRODUCT_DETAIL_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
   }
 };
