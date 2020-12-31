c9c6d0678dab43eaa2eae8486e20c98ef820869d
Piyush Raj <piyushrajkhg@gmail.com>
2020-12-31 17:52:31 +0530

    fixed api

--- frontend/src/redux/actions/cart/index.js
+++ frontend/src/redux/actions/cart/index.js
@@ -2,13 +2,12 @@ import axios from "axios";
 import { CART_ADD_ITEM, CART_REMOVE_ITEM } from "./actionTypes";
 
 export const addToCart = (productId, qty) => async (dispatch, getState) => {
-  let { data } = await axios.get(`/api/v1/product/${productId}`);
-  data = data.product;
+  let { data } = await axios.get(`/api/v1/products/${productId}`);
   dispatch({
     type: CART_ADD_ITEM,
     payload: {
       name: data.name,
-      image: data.images,
+      image: data.image,
       price: data.price,
       countInStock: data.countInStock,
       product: data._id,
