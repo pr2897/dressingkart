f158f8ffacfcf45983ccafe5dda1b16ce6e718c7
Piyush Raj <piyushrajkhg@gmail.com>
2020-12-31 14:52:25 +0530

    added product model and product router

---
+++ backend/routers/productRouter.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const expressAsyncHandler = require('express-async-handler');
+const Product = require('../models/productModel');
+
+const productRouter = express.Router();
+
+productRouter.get(
+  '/',
+  expressAsyncHandler(async (req, res) => {
+    const products = await Product.find({});
+    res.send({ products });
+  })
+);
+
+// productRouter.get(
+//   '/seed',
+//   expressAsyncHandler(async (req, res) => {
+//     const createdProducts = await Product.insertMany(data.products);
+//     res.send({ createdProducts });
+//   })
+// );
+
+productRouter.get(
+  '/:id',
+  expressAsyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (product) res.send(product);
+    else return res.status(404).send({ message: 'Product not found' });
+  })
+);
+
+module.exports = productRouter;
