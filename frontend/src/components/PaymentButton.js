a71888a01751aa04394f4d6ebf47824804dd39a7
Piyush Raj <piyushrajkhg@gmail.com>
2021-01-09 00:16:41 +0530

    added payment button component

---
+++ frontend/src/components/PaymentButton.js
@@ -0,0 +1,58 @@
+import React from "react";
+
+import GooglePayButton from "@google-pay/button-react";
+
+function PaymentButton({ amountToPay }) {
+  return (
+    <>
+      <GooglePayButton
+        environment="TEST"
+        paymentRequest={{
+          apiVersion: 2,
+          apiVersionMinor: 0,
+          allowedPaymentMethods: [
+            {
+              type: "CARD",
+              parameters: {
+                allowedAuthMethods: ["PAN_ONLY", "CRYPTOGRAM_3DS"],
+                allowedCardNetworks: ["MASTERCARD", "VISA"],
+              },
+              tokenizationSpecification: {
+                type: "PAYMENT_GATEWAY",
+                parameters: {
+                  gateway: "example",
+                  gatewayMerchantId: "exampleGatewayMerchantId",
+                },
+              },
+            },
+          ],
+          merchantInfo: {
+            merchantId: "12345678901234567890",
+            merchantName: "Demo Merchant",
+          },
+          transactionInfo: {
+            totalPriceStatus: "FINAL",
+            totalPriceLabel: "Total",
+            totalPrice: `${amountToPay}`,
+            currencyCode: "INR",
+            countryCode: "IN",
+          },
+          //   shippingAddressRequired: true,
+          callbackIntents: ["PAYMENT_AUTHORIZATION"],
+        }}
+        onLoadPaymentData={(paymentRequest) => {
+          console.log("Success", paymentRequest);
+        }}
+        onPaymentAuthorized={(paymentData) => {
+          console.log("Payment Authorized Success", paymentData);
+          return { transactionState: "SUCCESS" };
+        }}
+        existingPaymentMethodRequired="false"
+        buttonColor="black"
+        buttonType="buy"
+      />
+    </>
+  );
+}
+
+export default PaymentButton;
